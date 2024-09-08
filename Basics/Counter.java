155dc4866a3336f1bbb34e090631de9cae0d4379
AFamchankou <famchankou@gmail.com>
2024-09-08 10:07:57 +0200

    chore: init bloch examples

---
+++ Basics/Counter.java
@@ -0,0 +1,42 @@
+package Basics;
+
+public class Counter {
+    private int count;
+
+    public Counter() {}
+
+    public Counter(int initial) {
+        count = initial;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void increment() {
+        count++;
+    }
+
+    public void increment(int delta) {
+        count += delta;
+    }
+
+    public void reset() {
+        count = 0;
+    }
+
+    public static void main(String[] ars) {
+        Counter c;
+        c = new Counter();
+        c.increment();
+        c.increment(3);
+        int temp = c.getCount();
+        c.reset();
+
+        Counter d = new Counter(5);
+        d.increment();
+        Counter e = d;
+        temp = e.getCount();
+        e.increment(2);
+    }
+}
