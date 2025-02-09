e13fa103bf6aac432d88ce7523d91c2f3533d299
AFamchankou <famchankou@gmail.com>
2025-02-09 10:15:53 +0100

    chore: add methods ref

---
+++ MethodReference/PassMethodRef.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class PassMethodRef {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Navin", "Harsh", "John");
+        List<String> uNames = names.stream().map(String::toUpperCase).toList();
+
+        // System.out.println(uNames);
+        uNames.forEach(System.out::println);
+    }    
+}
